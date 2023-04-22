812bf0e5097fd8bab86e77a3df02f833f16b8e2b
Al Lamarre <al.lamarre@d2l.com>
2023-04-21 20:58:29 -0400

    try a simple blazor deploy

---
+++ cloudflare-pages-build.sh
@@ -0,0 +1,5 @@
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet publish src/MaestroBlazor -c Release -o output
